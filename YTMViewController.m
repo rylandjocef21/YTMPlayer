9640f6b576829a621c82305e7a01c56356aa2dcd
rylandjocef21 <rylandjocef21@gmail.com>
2026-08-27 21:05:31 +0800

    Update YTMViewController.m

--- YTMViewController.m
+++ YTMViewController.m
@@ -1,8 +1,8 @@
-#import <UIKit/UIKit.h>
+#import "YTMViewController.h"
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
-@interface YTMViewController : UIViewController <UITextFieldDelegate>
+@interface YTMViewController () <UITextFieldDelegate>
 @property (nonatomic, strong) UITextField *searchField;
 @property (nonatomic, strong) UIButton *playButton;
 @property (nonatomic, strong) AVPlayer *audioPlayer;
@@ -13,13 +13,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
-
+    
     self.searchField = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, self.view.frame.size.width - 40, 40)];
     self.searchField.placeholder = @"Enter YouTube Video ID...";
     self.searchField.backgroundColor = [UIColor whiteColor];
     self.searchField.layer.cornerRadius = 5.0;
     [self.view addSubview:self.searchField];
-
+    
     self.playButton = [UIButton buttonWithType:UIButtonTypeSystem];
     self.playButton.frame = CGRectMake(20, 140, self.view.frame.size.width - 40, 50);
     self.playButton.backgroundColor = [UIColor colorWithRed:0.9 green:0.1 blue:0.1 alpha:1.0];
@@ -27,11 +27,11 @@
     [self.playButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [self.playButton addTarget:self action:@selector(fetchAndPlayTrack) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.playButton];
-
+    
     NSError *categoryError = nil;
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&categoryError];
     [[AVAudioSession sharedInstance] setActive:YES error:nil];
-
+    
     [self setupRemoteCommandCenter];
 }
 
@@ -50,14 +50,14 @@
 - (void)fetchAndPlayTrack {
     NSString *videoId = self.searchField.text;
     if (videoId.length == 0) videoId = @"dQw4w9WgXcQ";
-
+    
     [self getStreamUrlFromInnerTube:videoId completion:^(NSString *streamUrl, NSString *title) {
         if (!streamUrl) return;
         dispatch_async(dispatch_get_main_queue(), ^{
             NSURL *url = [NSURL URLWithString:streamUrl];
             self.audioPlayer = [AVPlayer playerWithURL:url];
             [self.audioPlayer play];
-
+            
             NSDictionary *nowPlayingInfo = @{
                 MPMediaItemPropertyTitle: title ?: @"InnerTube Track",
                 MPNowPlayingInfoPropertyPlaybackRate: @(1.0)
@@ -72,7 +72,7 @@
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:apiURL];
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-
+    
     NSDictionary *payload = @{
         @"context": @{
             @"client": @{
@@ -82,22 +82,22 @@
         },
         @"videoId": videoId
     };
-
+    
     NSData *bodyData = [NSJSONSerialization dataWithJSONObject:payload options:0 error:nil];
     [request setHTTPBody:bodyData];
-
+    
     NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:request
         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
             if (error || !data) {
                 completion(nil, nil);
                 return;
             }
-
+            
             NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
             NSArray *formats = json[@"streamingData"][@"adaptiveFormats"];
             NSString *title = json[@"videoDetails"][@"title"];
             NSString *audioUrl = nil;
-
+            
             for (NSDictionary *format in formats) {
                 NSString *mimeType = format[@"mimeType"];
                 if ([mimeType containsString:@"audio/mp4"]) {
@@ -105,7 +105,7 @@
                     break;
                 }
             }
-
+            
             completion(audioUrl, title);
     }];
     [task resume];
