a1034057ae83afa3ce5a600e006e0fcf7b7bab68
rylandjocef21 <rylandjocef21@gmail.com>
2026-08-28 10:00:46 +0800

    Replace UI: Spotify-like player (search, history, persistent player bar)

--- YTMViewController.m
+++ YTMViewController.m
@@ -3,15 +3,48 @@
 #import "YTMTrackParser.h"
 #import "YTMPlayerManager.h"
 
+@interface TrackCell : UITableViewCell
+@property (nonatomic, strong) UIImageView *art;
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel *subtitleLabel;
+@end
+
+@implementation TrackCell
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)rid {
+    if (!(self = [super initWithStyle:style reuseIdentifier:rid])) return nil;
+    self.backgroundColor = [UIColor clearColor];
+    self.art = [[UIImageView alloc] initWithFrame:CGRectMake(12,8,56,56)];
+    self.art.layer.cornerRadius = 4;
+    self.art.clipsToBounds = YES;
+    [self.contentView addSubview:self.art];
+
+    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(80,10, self.contentView.bounds.size.width - 100, 22)];
+    self.titleLabel.font = [UIFont systemFontOfSize:16 weight:UIFontWeightSemibold];
+    self.titleLabel.textColor = [UIColor whiteColor];
+    [self.contentView addSubview:self.titleLabel];
+
+    self.subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(80,32, self.contentView.bounds.size.width - 100, 20)];
+    self.subtitleLabel.font = [UIFont systemFontOfSize:13];
+    self.subtitleLabel.textColor = [UIColor lightGrayColor];
+    [self.contentView addSubview:self.subtitleLabel];
+
+    return self;
+}
+@end
+
 @interface YTMViewController () <UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource>
-@property (nonatomic, strong) UISegmentedControl *sectionSegment;
+@property (nonatomic, strong) UISegmentedControl *segment;
 @property (nonatomic, strong) UISearchBar *searchBar;
 @property (nonatomic, strong) UITableView *tableView;
-@property (nonatomic, strong) UIView *nowPlayingView;
-@property (nonatomic, strong) UILabel *nowPlayingLabel;
+@property (nonatomic, strong) UIView *playerBar;
+@property (nonatomic, strong) UIImageView *playerArt;
+@property (nonatomic, strong) UILabel *playerTitle;
+@property (nonatomic, strong) UILabel *playerArtist;
 @property (nonatomic, strong) UIButton *playPauseButton;
-@property (nonatomic, strong) NSMutableArray<NSDictionary *> *searchResults;
-@property (nonatomic, strong) NSMutableArray<NSDictionary *> *historyResults;
+@property (nonatomic, strong) UISlider *progressSlider;
+
+@property (nonatomic, strong) NSMutableArray<NSDictionary *> *results;
+@property (nonatomic, strong) NSMutableArray<NSDictionary *> *history;
 @property (nonatomic, strong) YTMTrackParser *parser;
 @end
 
@@ -19,176 +52,180 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor colorWithRed:0.07 green:0.07 blue:0.07 alpha:1.0];
-    self.searchResults = [NSMutableArray array];
-    [self loadHistory];
+    self.view.backgroundColor = [UIColor colorWithWhite:0.05 alpha:1.0];
     self.parser = [YTMTrackParser new];
+    self.results = [NSMutableArray array];
+    [self loadHistory];
 
-    // UI
-    self.sectionSegment = [[UISegmentedControl alloc] initWithItems:@[@"Search",@"History"]];
-    self.sectionSegment.frame = CGRectMake(20,28,self.view.frame.size.width-40,30);
-    self.sectionSegment.selectedSegmentIndex = 0;
-    self.sectionSegment.tintColor = [UIColor colorWithRed:0.11 green:0.84 blue:0.38 alpha:1.0];
-    [self.sectionSegment addTarget:self action:@selector(segmentChanged:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:self.sectionSegment];
+    CGFloat top = 44;
+    self.segment = [[UISegmentedControl alloc] initWithItems:@[ @"Search", @"History" ]];
+    self.segment.selectedSegmentIndex = 0;
+    self.segment.tintColor = [UIColor colorWithRed:0.11 green:0.84 blue:0.38 alpha:1.0];
+    self.segment.frame = CGRectMake(12, top, self.view.bounds.size.width - 24, 30);
+    [self.segment addTarget:self action:@selector(segChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.segment];
 
-    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0,64,self.view.frame.size.width,44)];
+    self.searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, top + 44, self.view.bounds.size.width, 56)];
     self.searchBar.delegate = self;
-    self.searchBar.placeholder = @"Search songs or artists...";
+    self.searchBar.placeholder = @"Search songs or artists";
     self.searchBar.barStyle = UIBarStyleBlack;
-    self.searchBar.tintColor = [UIColor colorWithRed:0.11 green:0.84 blue:0.38 alpha:1.0];
+    self.searchBar.searchBarStyle = UISearchBarStyleMinimal;
     [self.view addSubview:self.searchBar];
 
-    CGFloat tableY = 108;
-    CGFloat tableHeight = self.view.frame.size.height - tableY - 60;
-    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0,tableY,self.view.frame.size.width,tableHeight) style:UITableViewStylePlain];
+    CGFloat tableY = CGRectGetMaxY(self.searchBar.frame);
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, tableY, self.view.bounds.size.width, self.view.bounds.size.height - tableY - 72) style:UITableViewStylePlain];
+    self.tableView.backgroundColor = [UIColor clearColor];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    self.tableView.backgroundColor = [UIColor clearColor];
-    self.tableView.separatorColor = [UIColor colorWithWhite:0.2 alpha:1.0];
+    [self.tableView registerClass:[TrackCell class] forCellReuseIdentifier:@"TrackCell"];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:self.tableView];
 
-    [self setupNowPlayingBar];
-    // Audio session + remote controls handled in YTMPlayerManager
+    [self setupPlayerBar];
+    // Initialize player manager
     [YTMPlayerManager sharedManager];
 }
 
-#pragma mark - UI Helpers
-- (void)setupNowPlayingBar {
-    CGFloat barY = self.view.frame.size.height - 60;
-    self.nowPlayingView = [[UIView alloc] initWithFrame:CGRectMake(0, barY, self.view.frame.size.width, 60)];
-    self.nowPlayingView.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:1.0];
+- (void)setupPlayerBar {
+    CGFloat h = 72;
+    CGFloat y = self.view.bounds.size.height - h;
+    self.playerBar = [[UIView alloc] initWithFrame:CGRectMake(0, y, self.view.bounds.size.width, h)];
+    self.playerBar.backgroundColor = [UIColor colorWithWhite:0.07 alpha:1.0];
+    self.playerBar.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleWidth;
 
-    self.nowPlayingLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 10, self.view.frame.size.width - 80, 40)];
-    self.nowPlayingLabel.textColor = [UIColor whiteColor];
-    self.nowPlayingLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightMedium];
-    self.nowPlayingLabel.text = @"Not Playing";
-    [self.nowPlayingView addSubview:self.nowPlayingLabel];
+    self.playerArt = [[UIImageView alloc] initWithFrame:CGRectMake(12, 8, 56, 56)];
+    self.playerArt.layer.cornerRadius = 4;
+    self.playerArt.clipsToBounds = YES;
+    [self.playerBar addSubview:self.playerArt];
 
-    self.playPauseButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    self.playPauseButton.frame = CGRectMake(self.view.frame.size.width - 50, 15, 30, 30);
-    [self.playPauseButton setTitle:@"▶" forState:UIControlStateNormal];
-    [self.playPauseButton setTitleColor:[UIColor colorWithRed:0.11 green:0.84 blue:0.38 alpha:1.0] forState:UIControlStateNormal];
-    [self.playPauseButton addTarget:self action:@selector(togglePlayPause) forControlEvents:UIControlEventTouchUpInside];
-    [self.nowPlayingView addSubview:self.playPauseButton];
+    self.playerTitle = [[UILabel alloc] initWithFrame:CGRectMake(80, 10, self.view.bounds.size.width - 170, 22)];
+    self.playerTitle.font = [UIFont systemFontOfSize:14 weight:UIFontWeightSemibold];
+    self.playerTitle.textColor = [UIColor whiteColor];
+    [self.playerBar addSubview:self.playerTitle];
 
-    [self.view addSubview:self.nowPlayingView];
-}
+    self.playerArtist = [[UILabel alloc] initWithFrame:CGRectMake(80, 32, self.view.bounds.size.width - 170, 18)];
+    self.playerArtist.font = [UIFont systemFontOfSize:12];
+    self.playerArtist.textColor = [UIColor lightGrayColor];
+    [self.playerBar addSubview:self.playerArtist];
 
-- (void)segmentChanged:(UISegmentedControl *)sender {
-    if (sender.selectedSegmentIndex == 0) {
-        self.searchBar.hidden = NO;
-        self.tableView.frame = CGRectMake(0,108,self.view.frame.size.width,self.view.frame.size.height - 168);
-    } else {
-        self.searchBar.hidden = YES;
-        self.tableView.frame = CGRectMake(0,64,self.view.frame.size.width,self.view.frame.size.height - 124);
-    }
-    [self.tableView reloadData];
+    self.playPauseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.playPauseButton.frame = CGRectMake(self.view.bounds.size.width - 56, 18, 44, 36);
+    [self.playPauseButton setTitle:@"▶︎" forState:UIControlStateNormal];
+    self.playPauseButton.titleLabel.font = [UIFont systemFontOfSize:28];
+    [self.playPauseButton addTarget:self action:@selector(togglePlay) forControlEvents:UIControlEventTouchUpInside];
+    [self.playerBar addSubview:self.playPauseButton];
+
+    self.progressSlider = [[UISlider alloc] initWithFrame:CGRectMake(80, 54, self.view.bounds.size.width - 160, 10)];
+    self.progressSlider.minimumValue = 0;
+    self.progressSlider.maximumValue = 1;
+    [self.playerBar addSubview:self.progressSlider];
+
+    [self.view addSubview:self.playerBar];
 }
 
 #pragma mark - Search
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSString *q = searchBar.text;
+    if (q.length == 0) return;
     [searchBar resignFirstResponder];
-    NSString *query = searchBar.text;
-    if (query.length == 0) return;
-    __unsafe_unretained typeof(self) wself = self;
-    [[YTMNetworkClient sharedClient] searchYouTubeMusic:query completion:^(NSDictionary * _Nullable json, NSError * _Nullable error) {
-        if (error || !json) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                // minimally show empty
-                [wself.searchResults removeAllObjects];
-                [wself.tableView reloadData];
-            });
-            return;
-        }
-        NSArray *tracks = [wself.parser parseTracksFromResponse:json];
+    __weak typeof(self) weak = self;
+    [[YTMNetworkClient sharedClient] searchYouTubeMusic:q completion:^(NSDictionary * _Nullable json, NSError * _Nullable error) {
         dispatch_async(dispatch_get_main_queue(), ^{
-            [wself.searchResults removeAllObjects];
-            [wself.searchResults addObjectsFromArray:tracks];
-            [wself.tableView reloadData];
+            if (error || !json) {
+                // show a tiny alert
+                UIAlertController *a = [UIAlertController alertControllerWithTitle:@"Search failed" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+                [a addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
+                [weak presentViewController:a animated:YES completion:nil];
+                return;
+            }
+            NSArray *tracks = [weak.parser parseTracksFromResponse:json];
+            [weak.results removeAllObjects];
+            [weak.results addObjectsFromArray:tracks];
+            [weak.tableView reloadData];
         });
     }];
 }
 
 #pragma mark - Table
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return (self.sectionSegment.selectedSegmentIndex == 0) ? self.searchResults.count : self.historyResults.count;
+    return (self.segment.selectedSegmentIndex == 0) ? self.results.count : self.history.count;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString *cellID = @"TrackCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
-        cell.backgroundColor = [UIColor clearColor];
-        cell.textLabel.textColor = [UIColor whiteColor];
-        cell.detailTextLabel.textColor = [UIColor lightGrayColor];
-    }
-    NSDictionary *track = (self.sectionSegment.selectedSegmentIndex == 0) ? self.searchResults[indexPath.row] : self.historyResults[indexPath.row];
-    cell.textLabel.text = track[@"title"];
-    cell.detailTextLabel.text = track[@"artist"];
+    TrackCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TrackCell" forIndexPath:indexPath];
+    NSDictionary *track = (self.segment.selectedSegmentIndex == 0) ? self.results[indexPath.row] : self.history[indexPath.row];
+    cell.titleLabel.text = track[@"title"] ?: @"Unknown";
+    cell.subtitleLabel.text = track[@"artist"] ?: @"";
+    // art: if track contains artworkUrl, load it async; otherwise set placeholder color
+    cell.art.backgroundColor = [UIColor darkGrayColor];
     return cell;
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    NSDictionary *selectedTrack = (self.sectionSegment.selectedSegmentIndex == 0) ? self.searchResults[indexPath.row] : self.historyResults[indexPath.row];
-    [self addToHistory:selectedTrack];
-    self.nowPlayingLabel.text = [NSString stringWithFormat:@"%@ - %@", selectedTrack[@"title"], selectedTrack[@"artist"]];
+    NSDictionary *track = (self.segment.selectedSegmentIndex == 0) ? self.results[indexPath.row] : self.history[indexPath.row];
+    [self addToHistory:track];
 
-    __unsafe_unretained typeof(self) wself = self;
-    [[YTMNetworkClient sharedClient] playerForVideoId:selectedTrack[@"videoId"] completion:^(NSDictionary * _Nullable json, NSError * _Nullable error) {
-        if (error || !json) {
-            // TODO: better UI feedback
-            return;
-        }
-        // Try to extract a playable URL (formats/adaptiveFormats)
-        NSArray *formats = json[@"streamingData"][@"adaptiveFormats"] ?: json[@"streamingData"][@"formats"];
-        NSString *playUrl = nil;
-        for (NSDictionary *f in formats) {
-            NSString *u = f[@"url"];
-            if (u && u.length > 0) {
-                playUrl = u; break;
+    self.playerTitle.text = track[@"title"];
+    self.playerArtist.text = track[@"artist"];
+
+    __weak typeof(self) weak = self;
+    [[YTMNetworkClient sharedClient] playerForVideoId:track[@"videoId"] completion:^(NSDictionary * _Nullable json, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (error || !json) {
+                UIAlertController *a = [UIAlertController alertControllerWithTitle:@"Playback error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
+                [a addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
+                [weak presentViewController:a animated:YES completion:nil];
+                return;
             }
-            // handle signatureCipher case: the client should not try to decipher; recommend proxy
-            NSString *cipher = f[@"signatureCipher"] ?: f[@"cipher"];
-            if (cipher && cipher.length > 0) {
-                // prefer server/proxy to resolve signature; skip here
-                continue;
+            NSArray *formats = json[@"streamingData"][@"adaptiveFormats"] ?: json[@"streamingData"][@"formats"];
+            NSString *playUrl = nil;
+            for (NSDictionary *f in formats) {
+                NSString *u = f[@"url"];
+                if (u.length) { playUrl = u; break; }
             }
-        }
-        if (playUrl) {
-            [[YTMPlayerManager sharedManager] playURLString:playUrl title:selectedTrack[@"title"] artist:selectedTrack[@"artist"]];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [wself.playPauseButton setTitle:@"⏸" forState:UIControlStateNormal];
-            });
-        } else {
-            // no usable URL on client — recommend proxy / show error
-        }
+            if (playUrl) {
+                [[YTMPlayerManager sharedManager] playURLString:playUrl title:track[@"title"] artist:track[@"artist"]];
+                [weak.playPauseButton setTitle:@"⏸" forState:UIControlStateNormal];
+            } else {
+                UIAlertController *a = [UIAlertController alertControllerWithTitle:@"No playable stream" message:@"This track requires a proxy to resolve the stream." preferredStyle:UIAlertControllerStyleAlert];
+                [a addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil]];
+                [weak presentViewController:a animated:YES completion:nil];
+            }
+        });
     }];
-}
-
-#pragma mark - Player controls
-- (void)togglePlayPause {
-    [[YTMPlayerManager sharedManager] togglePlayPause];
-    BOOL playing = [YTMPlayerManager sharedManager].isPlaying;
-    [self.playPauseButton setTitle:(playing ? @"⏸" : @"▶") forState:UIControlStateNormal];
 }
 
 #pragma mark - History
 - (void)loadHistory {
     NSArray *saved = [[NSUserDefaults standardUserDefaults] objectForKey:@"YTMPlayerHistory"];
-    if (saved) self.historyResults = [saved mutableCopy];
-    else self.historyResults = [NSMutableArray array];
+    if (saved) self.history = [saved mutableCopy];
+    else self.history = [NSMutableArray array];
 }
 - (void)addToHistory:(NSDictionary *)track {
-    for (NSInteger i = 0; i < self.historyResults.count; i++) {
-        if ([self.historyResults[i][@"videoId"] isEqualToString:track[@"videoId"]]) {
-            [self.historyResults removeObjectAtIndex:i];
+    if (!track) return;
+    for (NSInteger i=0;i<self.history.count;i++){
+        if ([self.history[i][@"videoId"] isEqualToString:track[@"videoId"]]) {
+            [self.history removeObjectAtIndex:i];
             break;
         }
     }
-    [self.historyResults insertObject:track atIndex:0];
-    [[NSUserDefaults standardUserDefaults] setObject:self.historyResults forKey:@"YTMPlayerHistory"];
+    [self.history insertObject:track atIndex:0];
+    [[NSUserDefaults standardUserDefaults] setObject:self.history forKey:@"YTMPlayerHistory"];
     [[NSUserDefaults standardUserDefaults] synchronize];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Player controls
+- (void)togglePlay {
+    YTMPlayerManager *mgr = [YTMPlayerManager sharedManager];
+    [mgr togglePlayPause];
+    BOOL playing = mgr.isPlaying;
+    [self.playPauseButton setTitle:(playing?@"⏸":@"▶︎") forState:UIControlStateNormal];
+}
+
+#pragma mark - Segment
+- (void)segChanged:(UISegmentedControl*)s {
+    self.searchBar.hidden = (s.selectedSegmentIndex != 0);
+    [self.tableView reloadData];
 }
 
 @end
